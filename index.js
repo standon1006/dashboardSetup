a175ed4d2f629745e4b77fd15a5049a72b5422a2
standon1006 <89518445+standon1006@users.noreply.github.com>
2021-10-24 21:56:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var datas = d3.csv("data.csv").then(function(data) {
+var datanew = d3.csv("data.csv", function(data) {
+				console.log(data);
+			});
 
-  // Format
-  data.forEach(function(d) {
-    d.value = +d.value;
-  });
+
+const datas = [
+    { language : "A", value : 47 },
+    { language : "B", value : 81 },
+    { language : "C", value : 33 },
+    { language : "D", value : 54.21 },
+    { language : "E", value : 41.78 },
+    { language : "F", value : 41.78 },
+    { language : "G", value : 41.78 },
+    { language : "H", value : 41.78 },
+    { language : "I", value : 41.78 },
+]
