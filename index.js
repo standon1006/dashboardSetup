8f0330055aff0aeac440cf9ed4821c9ff7f3d0ce
standon1006 <89518445+standon1006@users.noreply.github.com>
2021-10-24 22:47:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-var datanew = d3.csv("data.csv", function(data) {
-				console.log(data);
-			});
+d3.csv("data.csv", function(data) {
+				d3.select(#output").html(
+					  data.map(function(d){
+					return d.wk;
+			})
+			);
+});
 
 
-const datas = [
-    { language : "A", value : 47 },
-    { language : "B", value : 81 },
-    { language : "C", value : 33 },
-    { language : "D", value : 54.21 },
-    { language : "E", value : 41.78 },
-    { language : "F", value : 41.78 },
-    { language : "G", value : 41.78 },
-    { language : "H", value : 41.78 },
-    { language : "I", value : 41.78 },
-]
+
