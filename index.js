360c68bce79fdd1d8fd6edfb7a6846b95e626711
standon1006 <89518445+standon1006@users.noreply.github.com>
2021-10-25 09:17:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-d3.csv("data.csv", function(data) {
+d3.csv("data.csv", function(error, csv) {
+        if (error) return console.warn(error);
+
+        csv.forEach(function(d){ (d['value'] = +d['value']); });
+        console.log(data);
 				
 					
-					console.log(data);
+					
 			
 			
 });
